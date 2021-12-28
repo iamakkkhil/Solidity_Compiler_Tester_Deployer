821fa90fcbbf93ee16a9ab703b044efffac172ea
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-12-28 18:51:25 +0530

    First contract added v0.4.17 compiled

---
+++ compile.js
@@ -0,0 +1,11 @@
+// path is used to generate a valid path for the file inpedent of OS
+const path = require('path');
+// file system is used to read and write files
+const fs = require('fs');
+// solc is used to compile solidity files
+const solc = require('solc');
+
+const inboxPath = path.join(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
